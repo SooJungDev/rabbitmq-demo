daf950358b43f9cea718c811c78bc394cc9e4396
crystal <crystal2840@neowiz.com>
2020-08-10 19:45:30 +0900

    :fire: hello world 예제 실습

---
+++ src/main/java/com/example/rabbitmqdemo/RabbitAmqpTutorialsRunner.java
@@ -0,0 +1,22 @@
+package com.example.rabbitmqdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class RabbitAmqpTutorialsRunner implements CommandLineRunner {
+
+    @Value("${tutorial.client.duration:0}")
+    private int duration;
+
+    @Autowired
+    private ConfigurableApplicationContext ctx;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Ready ...runnig for " + duration + "me");
+        Thread.sleep(duration);
+        ctx.close();
+    }
+}
---
+++ src/main/java/com/example/rabbitmqdemo/config/TutlConfig.java
@@ -0,0 +1,31 @@
+package com.example.rabbitmqdemo.config;
+
+import com.example.rabbitmqdemo.TutlReceiver;
+import com.example.rabbitmqdemo.TutlSender;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+
+@Profile({"tutl", "hello-world"})
+@Configuration
+public class TutlConfig {
+
+    @Bean
+    public Queue hello() {
+        return new Queue("hello");
+    }
+
+    @Profile("receiver")
+    @Bean
+    public TutlReceiver receiver() {
+        return new TutlReceiver();
+    }
+
+    @Profile("sender")
+    @Bean
+    public TutlSender sender() {
+        return new TutlSender();
+    }
+}
